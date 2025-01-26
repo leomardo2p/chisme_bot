c67889d41aef85d11d4912e6f37acc72903d8eb1
leomardo2p <133141712+leomardo2p@users.noreply.github.com>
2025-01-26 09:19:35 -0500

    Add files via upload

--- bot_chismoso.py
+++ bot_chismoso.py
@@ -13,6 +13,8 @@ client = TelegramClient("bot",api_id,api_hash).start(bot_token=bot_token)
 
 print("Iniciando")
 
+lista=[]
+
 @client.on(events.NewMessage)
 async def handle_new_message(event):
     sender = await event.get_sender()
@@ -23,35 +25,78 @@ async def handle_new_message(event):
     #print(sender.id)
     #print(chat)
     
+    
     if(mesage=="/help"):
         await event.reply("Escribe en este formato el nuevo mensaje que vas a enviar: '/enviar Te tengo un chisme buenooo' , por favor recuerda el ' ' (espacio) despues del enviar, si tiene sugerencias use el comando /feedback , funciona exactamente igual que /enviar 😁")
+        
     elif(mesage[0:8] == "/enviar "):
-        index=mesage.find(" ")
-        text=mesage[index+1:]
-        text=text.replace("pinga","p*nga")
-        text=text.replace("cojone","coj*ne")
-        text=text.replace("bollo","b*llo")
-        text=text.replace("negga","n*gga")
-        text=text.replace("singao","sing*o")
-        await client.send_message("@Leonardo2004", text)
-        await event.reply("Mensaje enviado al administrador 🚀, si es aprobado se enviara al canal 😉")
+        if(event.message.media):
+            if isinstance(event.message.media, MessageMediaPhoto):
+                index=mesage.find(" ")
+                text=mesage[index+1:]
+                text=text.replace("pinga","p*nga")
+                text=text.replace("cojone","coj*ne")
+                text=text.replace("bollo","b*llo")
+                text=text.replace("negga","n*gga")
+                text=text.replace("singao","s*ngao")
+                text=text.replace("singar","s*ngar")
+                await client.send_message("@Leonardo2004", file=event.message.media.photo, message=text)
+                user={"id":event.sender.id,"mess":text}
+                lista.append(user)
+        else:
+            index=mesage.find(" ")
+            text=mesage[index+1:]
+            text=text.replace("pinga","p*nga")
+            text=text.replace("cojone","coj*ne")
+            text=text.replace("bollo","b*llo")
+            text=text.replace("negga","n*gga")
+            text=text.replace("singao","s*ngao")
+            text=text.replace("singar","s*ngar")
+            await client.send_message("@Leonardo2004", text)
+            await event.reply("Mensaje enviado al administrador 🚀, si es aprobado se enviara al canal 😉")
+            user={"id":event.sender.id,"mess":text}
+            lista.append(user)
+            
     elif(mesage=="/start"):
         await client.send_message(chat,"Hola! escribe /help para obtener ayuda de como enviar mensajes al canal")
+        
     elif(mesage=="/canal"):
         await event.reply("Link del canal de chismes y secretos 🤫: https://t.me/+FOTbPY4sSpw2ODMx")
+        
     elif(mesage=="/done" and event.message.reply_to and sender.id == 968663996):
         reply_to_msg_id = event.message.reply_to.reply_to_msg_id
         reply_to_peer_id = event.message.reply_to.reply_to_peer_id
         original_message = await client.get_messages(reply_to_peer_id, ids=reply_to_msg_id)
-        await client.send_message(chanel,original_message.message)
-        await event.reply("Mensaje enviado al canal")
+        text=original_message.message
+        if(original_message.media):
+            if isinstance(original_message.media, MessageMediaPhoto):
+                await client.send_message(chanel , file=original_message.media.photo , message=text)
+        else:
+            await client.send_message(chanel,original_message.message)
+            await event.reply("Mensaje enviado al canal")
+        
     elif(mesage[0:10] == "/feedback "):
         index=mesage.find(" ")
-        text=mesage[index+1:]
+        text="Mensaje de feedback: "
+        text=text + mesage[index+1:]
         await client.send_message("@Leonardo2004", text )
         await event.reply("Mensaje enviado al administrador, gracias por la sugerencia 😁")
+        
+    elif(mesage[0:4] == "/no " and event.message.reply_to and sender.id == 968663996 ):
+        reply_to_msg_id = event.message.reply_to.reply_to_msg_id
+        reply_to_peer_id = event.message.reply_to.reply_to_peer_id
+        original_message = await client.get_messages(reply_to_peer_id, ids=reply_to_msg_id)
+        text=original_message.message
+        index=mesage.find(" ")
+        mensage="El mensaje: " + text + " no ha sido enviado porque: " + mesage[index+1:]
+        for user in lista:
+            if(user["mess"]==text):
+                id=user["id"]
+                await client.send_message(id,mensage)
+                await event.reply("Mensaje enviado")
+        
     else:
-        await client.send_message(chat,"Formato del mensaje incorrecto")
+        await client.send_message(chat,"Formato del mensaje incorrecto, si tiene dudas presione /help")
          
 with client:
     client.run_until_disconnected()
